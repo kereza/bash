5cbd1ba2f557f71a3c31aad0047f961d057413c6
kereza <kereza85@gmail.com>
2014-04-09 16:59:42 +0300

    Update newdb.sh

--- newdb.sh
+++ newdb.sh
@@ -15,7 +15,8 @@ function newdb ()
 	db=$1\_$end
 	mysql -e "create database $db; GRANT ALL PRIVILEGES ON $db.* TO $db@localhost IDENTIFIED BY '$pass';FLUSH PRIVILEGES;"
 	/usr/local/cpanel/bin/dbmaptool  $1 --type mysql --dbusers "$db" --dbs "$db"
-			
+	rm -rf /var/cpanel/databases/$1.yaml
+	/usr/local/cpanel/bin/setupdbmap $1 >/dev/null 2>&1
 	echo "------------------------------------------"
 	echo " Database has been created successfully "
 	echo "------------------------------------------"
